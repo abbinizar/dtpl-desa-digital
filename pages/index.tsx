4317fdd4375fb85c611ddf8b6551879b64c10e1a
Abbi Nizar <abbinizarm@gmail.com>
2022-10-05 22:23:04 +0700

    init

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className='flex w-full justify-center h-screen items-center bg-slate-800'>
+      <h1 className="text-3xl font-bold underline decoration-pink-500 underline-offset-2">DTPL - Desa Digital</h1>
+    </div>
+  )
+}
+
+export default Home
